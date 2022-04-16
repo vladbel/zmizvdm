06159edba4f6651e12294abf30a5fe4287bf917a
Vladimir Beliaev <vbeliaev@revspringinc.com>
2022-04-16 10:49:22 -0500

    cli: initial commit

---
+++ cli/cli/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zmizvdm CLI ...");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    // process command
+                    Console.WriteLine($"command handled: {input}");
+                }
+            }
+
+            
+        }
+    }
+}
