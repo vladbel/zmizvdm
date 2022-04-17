83ccf5e4e84330ff3f31469a5bbb43a5f8db45f0
Vladimir Beliaev <vbeliaev@revspringinc.com>
2022-04-17 17:08:28 -0500

    1) Models: initial commit

---
+++ src/Models/Enums/TransactionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public enum TransactionType
+    {
+        Undefined,
+        Buy,
+        Sell,
+        Dividend
+    }
+}
