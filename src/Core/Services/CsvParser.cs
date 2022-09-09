039b9c70c5adda1c902db20b287da9aaefd8a6a3
Vladimir Beliaev <vbeliaev@revspringinc.com>
2022-09-09 15:26:50 -0500

    core: initial transaction parser implementation

---
+++ src/Core/Services/CsvParser.cs
@@ -0,0 +1,59 @@
+﻿using Core.Models;
+using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Services
+{
+    public class CsvParser : ITransactionParser
+    {
+        private const int SYMBOL_INDEX = 2;
+        private const int PRICE_INDEX = 6;
+        private const int QUANTITY_INDEX = 5;
+        private const int TOTAL_AMOUNT_INDEX = 10;
+        private const int RUN_DATE_INDEX = 0;
+        private const int TRANSACTION_DESCRIPTION_INDEX = 1;
+
+        /// <summary>
+        ///0-     09/06/2022,                                                         Run Date
+        ///1-     YOU BOUGHT VANGUARD INDEX FUNDS S&P 500 ETF USD (VOO) (Cash),       Action
+        ///2-     VOO,                                                                Symbol
+        ///3-     VANGUARD INDEX FUNDS S&P 500 ETF USD,                               Security Description
+        ///4-     Cash,                                                               Security Type
+        ///5-     1,                                                                  Quantity
+        ///6-    357.99,                                                             Price ($)
+        ///7-    ,                                                                   Commission
+        ///8-    ,                                                                   Fees ($)
+        ///9-    ,                                                                   Accrued Interest ($)
+        ///10-   -357.99,                                                            Amount ($)
+        ///11-   09/08/2022                                                          Settlement Date
+        /// </summary>
+        /// <param name="transactionRecord"></param>
+        /// <returns></returns>
+        public TransactionBase Parse(string transactionRecord)
+        {
+            var parts = transactionRecord.Split(',');
+            var transaction = new TransactionBase(
+                ticker: parts[SYMBOL_INDEX].Trim(),
+                price: decimal.Parse(parts[PRICE_INDEX]),
+                quantity: int.Parse(parts[QUANTITY_INDEX]),
+                total: decimal.Parse(parts[TOTAL_AMOUNT_INDEX]),
+                dateTime: DateTime.Parse(parts[RUN_DATE_INDEX]),
+                transactionType: ParseType(parts[TRANSACTION_DESCRIPTION_INDEX])) ;
+
+            return transaction;
+        }
+
+        private TransactionType ParseType (string description)
+        {
+            var transactionType = TransactionType.Undefined;
+            if (description.ToUpper().Contains("YOU BOUGHT"))
+            {
+                transactionType = TransactionType.Buy;
+            }
+
+            return transactionType;
+        }
+    }
+}
---
+++ src/Core/Services/ITransactionParser.cs
@@ -0,0 +1,9 @@
+﻿using Core.Models;
+
+namespace Core.Services
+{
+    public interface ITransactionParser
+    {
+        TransactionBase Parse(string transactionRecord);
+    }
+}
